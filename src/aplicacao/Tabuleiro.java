16e78ddd1f4955c9f134a66dde6312d8a25e5f57
juan silva <juanportela100@gmail.com>
2025-02-13 01:08:33 -0300

    Posição

--- src/aplicacao/Tabuleiro.java
+++ src/aplicacao/Tabuleiro.java
@@ -1,10 +1,15 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Tabuleiro {
 
 	public static void main(String[] args) {
 
 
+		Posicao pos = new Posicao(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
