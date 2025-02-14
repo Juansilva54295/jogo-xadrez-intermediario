e71588d8a3c1769cd46b4a3fcca80cfbed1da0b8
juan silva <juanportela100@gmail.com>
2025-02-13 23:36:25 -0300

    Movimentos possiveis Rei

--- src/xadrez/pecas/Rei.java
+++ src/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import tabuleiro.jogo.Posicao;
 import tabuleiro.jogo.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -15,9 +16,72 @@ public class Rei extends PecaXadrez {
 		return "R";
 	}
 
+	private boolean podemovimentar(Posicao posicao) {
+		PecaXadrez p = ( PecaXadrez)getTabuleiro().peca(posicao);
+		return p == null || p.getCor() != getCor();
+	}
+	
 	@Override
 	public boolean[][] movimentosPosiveis() {
 		boolean [][] mat = new boolean [getTabuleiro().getLinhas()][getTabuleiro().getColuna()];
+		Posicao p = new Posicao(0, 0);
+
+		// acima
+
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna());
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// abaixo
+		
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna());
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// esquerda
+		
+		p.setValues(posicao.getLinha(), posicao.getColuna() - 1);
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// direita
+		
+		p.setValues(posicao.getLinha(), posicao.getColuna() + 1);
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// noroeste
+		
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// nordeste
+		
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// sudoeste
+		
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// sudeste
+		
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posiexistente(p) && !getTabuleiro().haumaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
 		return mat;
 	}
 
